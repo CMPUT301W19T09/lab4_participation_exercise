49f062b5d1de580facb574e9d2435b9dcced5947
Tymoore Jamal <tymoore@ualberta.ca>
2019-01-31 18:16:33 -0700

    Some godly piece of code that would make James Miller Proud

---
+++ app/src/main/java/com/example/lslevins/lab4_exercise/Diamond.java
@@ -0,0 +1,11 @@
+package com.example.lslevins.lab4_exercise;
+
+public class Diamond extends Shape {
+    Diamond(){
+        super();
+    }
+
+    public String getTheTruth() {
+        return "Tymoore is as bright as a diamond.";
+    }
+}
