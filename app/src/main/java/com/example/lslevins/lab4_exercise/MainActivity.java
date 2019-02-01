9cb11d0f67fc0015389a5592cb967e8d5f0c40f8
LukeSlev <lslevins@ualberta.ca>
2019-01-31 18:08:09 -0700

    Here's some nice files for you all I hope you enjoy them :(

---
+++ app/src/main/java/com/example/lslevins/lab4_exercise/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.lslevins.lab4_exercise;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
